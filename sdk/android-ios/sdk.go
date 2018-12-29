83022d3efc57e45ce8f22cb00664d744c7ecab8e
arraykeys@gmail.com <arraykeys@gmail.com>
2018-12-29 13:29:45 +0800

    v6.8

--- sdk/android-ios/sdk.go
+++ sdk/android-ios/sdk.go
@@ -10,6 +10,7 @@ import (
 	"path/filepath"
 	"runtime/pprof"
 	"strings"
+	"sync"
 
 	"github.com/snail007/goproxy/core/lib/kcpcfg"
 	encryptconn "github.com/snail007/goproxy/core/lib/transport/encrypt"
@@ -34,7 +35,9 @@ var (
 	app *kingpin.Application
 	cpuProfilingFile, memProfilingFile, blockProfilingFile,
 	goroutineProfilingFile, threadcreateProfilingFile *os.File
-	isProfiling bool
+	isProfiling   bool
+	profilingLock = &sync.Mutex{}
+	startStopLock = &sync.Mutex{}
 )
 
 type LogCallback interface {
@@ -475,6 +478,8 @@ func StartWithLog(serviceID, serviceArgsStr string, loggerCallback LogCallback) 
 }
 
 func Stop(serviceID string) {
+	startStopLock.Lock()
+	defer startStopLock.Unlock()
 	services.Stop(serviceID)
 }
 
@@ -482,6 +487,8 @@ func Version() string {
 	return SDK_VERSION
 }
 func StartProfiling(storePath string) {
+	profilingLock.Lock()
+	defer profilingLock.Unlock()
 	if !isProfiling {
 		isProfiling = true
 		if storePath == "" {
@@ -496,6 +503,8 @@ func StartProfiling(storePath string) {
 	}
 }
 func StopProfiling() {
+	profilingLock.Lock()
+	defer profilingLock.Unlock()
 	if isProfiling {
 		isProfiling = false
 		pprof.StopCPUProfile()
@@ -507,6 +516,11 @@ func StopProfiling() {
 		block.WriteTo(blockProfilingFile, 1)
 		threadcreate := pprof.Lookup("threadcreate")
 		threadcreate.WriteTo(threadcreateProfilingFile, 1)
+		//close
+		goroutineProfilingFile.Close()
+		memProfilingFile.Close()
+		blockProfilingFile.Close()
+		threadcreateProfilingFile.Close()
 	}
 
 }
