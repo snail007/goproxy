8fbf7229f2bd46d0e0ac91951b832c4883483d6f
arraykeys <arraykeys@gmail.com>
2020-04-21 14:50:47 +0800

    no message

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 F="proxy-linux-amd64.tar.gz"
-
+manual="https://snail007.github.io/goproxy/manual/"
 set -e
-
-# #install proxy
-tar zxvf $F
+WORKDIR="/tmp/proxy"
+rm -rf $WORKDIR
+mkdir $WORKDIR
+cp $F $WORKDIR
+cd /tmp/proxy
+echo -e ">>> installing ... \n"
+tar zxvf $F >/dev/null
+set +e
+killall -9 proxy >/dev/null 2>&1
+set -e
 cp -f proxy /usr/bin/
 chmod +x /usr/bin/proxy
 if [ ! -e /etc/proxy ]; then
@@ -12,11 +19,14 @@ if [ ! -e /etc/proxy ]; then
     cp blocked /etc/proxy
     cp direct  /etc/proxy
 fi
-
 if [ ! -e /etc/proxy/proxy.crt ]; then
     cd /etc/proxy/
-    proxy keygen >/dev/null 2>&1 
+    proxy keygen -C proxy >/dev/null 2>&1 
 fi
 rm -rf /tmp/proxy
-echo "install done"
-proxy help
+version=`proxy --version 2>&1`
+echo  -e ">>> install done, thanks for using snail007/goproxy $version\n"
+echo  -e ">>> install path /usr/bin/proxy\n"
+echo  -e ">>> configuration path /etc/proxy\n"
+echo  -e ">>> uninstall just exec : rm /usr/bin/proxy && rm -rf /etc/proxy\n"
+echo  -e ">>> How to using? Please visit : $manual\n"
